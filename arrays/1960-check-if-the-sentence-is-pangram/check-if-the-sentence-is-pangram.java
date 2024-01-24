fd336d35f5cea9a01d5739b0d4404ad410664163
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-01-24 16:53:03 +0530

    Time: 2 ms (73.34%) | Memory: 41.6 MB (35.21%) - LeetSync

--- arrays/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ arrays/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int[] alphabet = new int[26]; // Create an integer array to represent the count of each letter in the alphabet
+        boolean[] alphabet = new boolean[26];
 
-        for (char c : sentence.toCharArray()) { // Iterate through each character in the sentence
-            int index = Character.toLowerCase(c) - 'a'; // Calculate the index of the character in the alphabet array
-            alphabet[index]++; // Increment the count at the corresponding index in the alphabet array
+        for(char c : sentence.toCharArray()){
+            int index = Character.toLowerCase(c) - 'a';
+
+            alphabet[index] = true;
+
         }
-
-        for (int count : alphabet) { // Iterate through each element in the alphabet array
-            if (count == 0) { // If any letter is missing (count is 0), return false
+        for(boolean letter: alphabet){
+            if(!letter){
                 return false;
             }
         }
-
-        return true; // If all letters are present (count > 0), return true
+        return true;
     }
 }
