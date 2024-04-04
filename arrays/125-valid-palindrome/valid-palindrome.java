17da8e0f69f4c88f3c6bbf4b4e4dcb0ac95ca171
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-04-04 11:27:47 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (58.47%) - LeetSync

--- arrays/125-valid-palindrome/valid-palindrome.java
+++ arrays/125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String fixedString = "";
-        for(char c: s.toCharArray()){
-            if(Character.isDigit(c) || Character.isLetter(c)){
-                fixedString += c;
-            }
-        }
-        fixedString = fixedString.toLowerCase();
-
-        int left = 0;
-        int right = fixedString.length()-1;
-        while(left <= right){
-            if(fixedString.charAt(left) != fixedString.charAt(right)){
+        char[] arr=s.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            while((i<=arr.length-1)&&!((arr[i]>='a'&& arr[i]<='z')||(arr[i]>='A'&& arr[i]<='Z')||(arr[i]>='0'&& arr[i]<='9')))
+                i++;
+            while((j>=0)&&!((arr[j]>='a'&& arr[j]<='z')||(arr[j]>='A'&& arr[j]<='Z')||(arr[j]>='0'&& arr[j]<='9')))
+                j--;
+            if(i>=j)
+                break;
+            if(arr[i]>='A'&& arr[i]<='Z')
+                arr[i]+='a'-'A';
+            if(arr[j]>='A'&& arr[j]<='Z')
+                arr[j]+='a'-'A';
+            if(arr[i++]!=arr[j--])
                 return false;
-            }
-            left++;
-            right--;
         }
         return true;
     }
