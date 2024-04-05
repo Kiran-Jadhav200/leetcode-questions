ecf102dff9006220d4b7a6fdb6e1d0b949c04b9b
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-04-05 17:49:11 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (77.06%) - LeetSync

---
+++ arrays/1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(!sb.isEmpty() && Math.abs(c - sb.charAt(sb.length()-1)) == 32)
+                sb.deleteCharAt(sb.length()-1);
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+
+}
