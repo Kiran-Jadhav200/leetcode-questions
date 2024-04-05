f8356534cb94e3424e5ca3ae092c42708cd53905
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-04-05 19:55:11 +0530

    Time: 7 ms (67.60%) | Memory: 42.7 MB (86.87%) - LeetSync

--- arrays/151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ arrays/151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,8 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
+        //removes leading and trailing spaces
         s = s.trim();
-
-       List<String>wordList =  Arrays.asList(s.split("\\s+"));
+        //split-> divide the string and puts element into array on the 
+        //delimiter condition
+       List<String>wordList =  Arrays.asList(s.split(" +"));
       // System.out.println(wordList);
        Collections.reverse(wordList);
        //System.out.println(wordList);
