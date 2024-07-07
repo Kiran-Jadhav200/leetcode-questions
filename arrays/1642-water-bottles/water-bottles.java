c5c30bf40426b52a619ce43f0471db798f0d085c
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-07-07 08:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (38.70%) - LeetSync

---
+++ arrays/1642-water-bottles/water-bottles.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+      int ans= numBottles + (numBottles) / (numExchange - 1);
+        if((numBottles) %(numExchange - 1)==0)
+            ans--;
+        return ans;
+     
+    }
+}
