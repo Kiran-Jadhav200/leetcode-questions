8c643ee5dbf2fde0e36d1fb98b86fe8f07827e47
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-07-08 09:34:04 +0530

    Time: 47 ms (16.53%) | Memory: 45.1 MB (11.98%) - LeetSync

--- arrays/1-two-sum/two-sum.java
+++ arrays/1-two-sum/two-sum.java
@@ -1,12 +1,30 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i<nums.length; i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[]{i, j};
-                }
-            }
+    public int[] twoSum(int[] arr, int target) {
+       
+    
+		int result[]=new int[2];
+		
+        int i = 0;
+        int j = i+1;
+
+        while( i < arr.length && j< arr.length){
+        
+        if(arr[i] + arr[j] == target){
+                    
+                   result[0] = i;
+                    result[1] = j;
+                    break;
         }
-        throw new IllegalArgumentException("No two sum solution");
-    }
+        j++;
+        if(j == arr.length){
+           i++;
+            j = i+1;
+            
+        }
+
+
+        }
+		
+		return result;
+}
 }
