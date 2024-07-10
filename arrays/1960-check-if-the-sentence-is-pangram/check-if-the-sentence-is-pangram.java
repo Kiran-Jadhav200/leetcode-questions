5ab1caf69c46a7369816f9091df54acf63379f77
Kiran Jadhav <89592054+Kiran-Jadhav200@users.noreply.github.com>
2024-07-10 10:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (36.19%) - LeetSync

--- arrays/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ arrays/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,18 +1,18 @@
 class Solution {
-    public boolean checkIfPangram(String sentence) {
-        boolean[] alphabet = new boolean[26];
-
-        for(char c : sentence.toCharArray()){
-            int index = Character.toLowerCase(c) - 'a';
-
-            alphabet[index] = true;
-
-        }
-        for(boolean letter: alphabet){
-            if(!letter){
-                return false;
+    public boolean checkIfPangram(String sec) {
+        boolean b= true;
+        if(sec.length()<26) b = false;
+        else{
+            for(char ch ='a' ;ch<='z'; ch++){
+                if(sec.contains(String.valueOf(ch))){
+                    b =true;
+                }
+                else{
+                    b = false;
+                    break;
+                }
             }
         }
-        return true;
+        return b;
     }
 }
